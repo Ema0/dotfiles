8c8c7379a38fb6a938110d2712df82f992d5b76b
Ema0 <ema.tironi@gmail.com>
2016-12-11 20:04:57 +0100

    Switched to polybar

--- Programs/mem.c
+++ Programs/mem.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/sysinfo.h>
 void main()
 {
 FILE *fp;
 char output[100];
 char s[5];
 char end[20];
+int sysinfo(struct sysinfo *info);
+/*bc -l <<< ((4050764/1024 - $(cat /proc/meminfo | grep -m 1 MemAvai | awk '{print $2}')/1024))
+*/
 fp = popen("/bin/free -h", "r");
 fgets(output, sizeof(output)-1, fp);
 fgets(output, sizeof(output)-1, fp);
